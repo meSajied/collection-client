ea34e3884f1e39a3bfd6f5e6609477e894247096
meSajied <sajiedshahyousuf@gmail.com>
2024-08-22 21:24:35 +0600

    added collection

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080"
+  baseURL: "https://collection-server-n34s.onrender.com"
 })
 
 export {axiosInstance}
