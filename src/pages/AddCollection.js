5e0052f350f56850b348f80f351eea7b1b7bcf2d
meSajied <sajiedshahyousuf@gmail.com>
2024-08-25 12:19:52 +0600

    fix: submit collection

--- src/pages/AddCollection.js
+++ src/pages/AddCollection.js
@@ -1,24 +1,20 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import { axiosInstance } from "../axiosInstance";
 import {useAuth} from "../account/Authentication";
 
 const AddCollection = () => {
+  const {user} = useAuth();
   const [collectionName, setCollectionName] = useState("");
-  const [username, setUsername] = useState("");
+  const [description, setDescription] = useState("");
   const [categories, setCategories] = useState([]);
   const [file, setFile] = useState(null);
   const [categoryInput, setCategoryInput] = useState("");
 
-  const {user} = useAuth();
-
-  // FormData state
-  const [formData, setFormData] = useState(new FormData());
+  const [form, setForm] = useState({collection:{}, file: null})
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     setFile(file);
-    formData.set("file", file);
-    setFormData(formData);
   };
 
   const handleCategoryChange = (e) => {
@@ -26,14 +22,12 @@ const AddCollection = () => {
   };
 
   const addCategory = () => {
-    if (categoryInput.trim()) {
-      const updatedCategories = [...categories, categoryInput.trim()];
+    if(categoryInput.trim()) {
+      const nw = {name: categoryInput.trim()}
+      const updatedCategories = [...categories, nw];
       setCategories(updatedCategories);
 
-      // Update categories in FormData
-      formData.set("categories", JSON.stringify(updatedCategories));
-      setFormData(formData);
-
+      console.log(categories)
       setCategoryInput("");
     }
   };
@@ -41,19 +35,28 @@ const AddCollection = () => {
   const handleNameChange = (e) => {
     const name = e.target.value;
     setCollectionName(name);
-    formData.set("name", name);
-    setFormData(formData);
+
   };
 
-  const handleUsernameChange = (e) => {
-    const username = e.target.value;
-    setUsername(username);
-    formData.set("username", username);
-    setFormData(formData);
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const collection = new FormData();
+
+    collection.append("collectionName", collectionName);
+    collection.append("description", description);
+    collection.append("categories", categories);
+    collection.append("username", user.username);
+
+    const formData = new FormData();
+    formData.append("collection", new Blob([JSON.stringify(collection)], {
+      type: "application/json"
+    }));
+    formData.append("file", file);
 
     try {
       const response = await axiosInstance.post("/collections/", formData, {
@@ -66,7 +69,12 @@ const AddCollection = () => {
     } catch (error) {
       console.error("Error adding collection:", error);
     }
+
   };
+
+  useEffect(() => {
+    console.log(form);
+  }, [form]);
 
   return (
       <form onSubmit={handleSubmit} className="p-8 max-w-lg mx-auto bg-white shadow-lg rounded-md">
@@ -85,11 +93,14 @@ const AddCollection = () => {
         </div>
 
         <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="description">
+            Description
+          </label>
           <input
-              type="hidden"
-              id="username"
-              value={user.username}
-              onChange={handleUsernameChange}
+              type="test"
+              id="description"
+              value={description}
+              onChange={handleDescriptionChange}
               className="w-full px-3 py-2 border rounded-md"
               required
           />
@@ -116,11 +127,13 @@ const AddCollection = () => {
             </button>
           </div>
           <ul className="mt-2">
+            {/*
             {categories.map((category, index) => (
                 <li key={index} className="bg-gray-200 inline-block px-3 py-1 rounded-full text-sm font-medium text-gray-700 mr-2 mt-1">
                   {category}
                 </li>
             ))}
+            */}
           </ul>
         </div>
 
